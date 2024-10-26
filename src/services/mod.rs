b74a00e8175baf940fa834daf45afe99b45b0571
Threated <jan2001.07@gmail.com>
2024-10-26 19:05:33 +0200

    feat: clean up service creation

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,22 +1,27 @@
-use std::any::TypeId;
-
 use crate::{dep_map::ServiceMap, Config};
 
 pub mod beam;
 pub mod focus;
 
-pub trait Service: ToCompose {
+pub trait Service: ToCompose + 'static {
     type Inputs: Service + 'static;
 
     fn from_config(conf: &Config, inputs: &mut Self::Inputs) -> Self
     where
         Self: Sized;
 
-    fn deps() -> Vec<TypeId>
+    fn make<'services>(conf: &Config, deps: &'services mut ServiceMap) -> &'services mut Self
     where
         Self: Sized,
     {
-        <Self::Inputs>::deps()
+        // Workaround for problem case #3
+        // https://smallcultfollowing.com/babysteps/blog/2016/04/27/non-lexical-lifetimes-introduction/
+        if deps.contains::<Self>() {
+            return deps.get_mut().unwrap();
+        }
+        let this = Self::from_config(conf, Self::Inputs::make(conf, deps));
+        deps.insert(this);
+        deps.get_mut().unwrap()
     }
 }
 
@@ -27,8 +32,15 @@ impl Service for () {
         ()
     }
 
-    fn deps() -> Vec<TypeId> {
-        Vec::with_capacity(0)
+    fn make<'a>(_conf: &Config, _deps: &'a mut ServiceMap) -> &'a mut Self
+    where
+        Self: Sized,
+    {
+        static mut THIS: () = ();
+        #[allow(static_mut_refs)]
+        unsafe {
+            &mut THIS
+        }
     }
 }
 
@@ -60,118 +72,16 @@ where
             T2::from_config(conf, &mut inputs.1),
         )
     }
-
-    fn deps() -> Vec<TypeId> {
-        let mut d = T::deps();
-        d.extend(T2::deps());
-        d
-    }
-}
-
-trait ServiceMaker {
-    fn make(conf: &Config, deps: &mut ServiceMap) -> Self;
-}
-
-// TODO: Maybe make this part of the Service trait and on the tuple impls override to not polute map
-// nevermind does not work because of the lookup maybe we can mark them some other way tho?
-impl<T: Service> ServiceMaker for T {
-    fn make(conf: &Config, deps: &mut ServiceMap) -> Self {
-        if let Some(inputs) = deps.get_mut() {
-            T::from_config(conf, inputs)
-        } else {
-            let inputs = T::Inputs::make(conf, deps);
-            deps.insert(inputs);
-            T::from_config(conf, deps.get_mut().unwrap())
-        }
-    }
 }
 
 pub fn make_services<T: Service + 'static>(conf: &Config) -> ServiceMap {
     let mut services = ServiceMap::default();
-    services.insert(());
-    let t = T::make(conf, &mut services);
-    services.insert(t);
-    services.remove::<()>();
+    T::make(conf, &mut services);
     services
 }
 
-// impl<O> Service for dyn Fn(&Config) -> O {
-//     type Inputs = ();
-//     type Output = O;
-//
-//     fn from_config(
-//         &self,
-//         conf: &Config,
-//         _inputs: &mut <Self::Inputs as Service>::Output,
-//     ) -> Self::Output {
-//         self(conf)
-//     }
-// }
-//
-// impl<O, A1> Service for dyn Fn(&Config, &mut A1::Output, PhantomData<A1>) -> O
-// where
-//     A1: Service,
-// {
-//     type Inputs = A1;
-//     type Output = O;
-//
-//     fn from_config(
-//         &self,
-//         conf: &Config,
-//         inputs: &mut <Self::Inputs as Service>::Output,
-//     ) -> Self::Output {
-//         self(conf, inputs, PhantomData)
-//     }
-// }
-//
-// impl<O, A1, A2> Service
-//     for dyn Fn(&Config, &mut A1::Output, &mut A2::Output, PhantomData<(A1, A2)>) -> O
-// where
-//     A1: Service,
-//     A2: Service,
-// {
-//     type Inputs = (A1, A2);
-//     type Output = O;
-//
-//     fn from_config(
-//         &self,
-//         conf: &Config,
-//         inputs: &mut <Self::Inputs as Service>::Output,
-//     ) -> Self::Output {
-//         self(conf, &mut inputs.0, &mut inputs.1, PhantomData)
-//     }
-// }
-
-// impl<T, D1, S> Service<(D1,)> for T
-// where
-//     T: Fn(&Config, D1) -> S,
-//     D1: 'static,
-//     S: ToCompose + 'static,
-// {
-//     type Output = S;
-//
-//     fn from_config(&self, conf: &Config, deps: &mut ServiceMap) -> S {
-//         self(conf, deps.get_mut().unwrap())
-//     }
-//
-//     fn dependecies(&self) -> Vec<TypeId> {
-//         vec![std::any::TypeId::of::<D1>()]
-//     }
-// }
-//
-// impl<S: ToCompose + 'static> Service for fn(&Config) -> S {
-//     fn from_config(&self, conf: &Config, _deps: &mut ServiceMap) -> Box<dyn ToCompose> {
-//         Box::new(self(conf))
-//     }
-//
-//     fn dependecies(&self) -> Vec<TypeId> {
-//         Vec::new()
-//     }
-// }
-//
 pub trait ToCompose {
     // TODO: Always quote secrets but just replace this with jinja templates
     // Acutal issue is $ in pws
     fn to_compose(&self) -> serde_yaml::Value;
 }
-
