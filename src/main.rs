ab45a59516cb198f340b01842ae8a945eb0bd510
janskiba <jan.skiba@dkfz-heidelberg.de>
2025-03-10 15:40:51 +0000

    chore: add example config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use serde::Deserialize;
 use services::{beam::DktkBroker, focus::Focus};
 use url::Url;
 mod dep_map;
@@ -6,45 +8,36 @@ mod modules;
 mod services;
 mod utils;
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "UPPERCASE")]
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
 struct Config {
-    project: Project,
     site_id: String,
-    http_proxy_url: Option<Url>,
-    https_proxy_url: Option<Url>,
+    http_proxy: Option<Url>,
+    https_proxy: Option<Url>,
+    ccp: Option<CcpConfig>,
+    // TODO Actual structs
+    bbmri: Option<HashMap<String, toml::Value>>,
+    // etc..
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
-enum Project {
-    Ccp,
-    Bbmri,
-    Minimal,
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+struct CcpConfig {
+    a: String,
+    c: Vec<String>,
+    d: HashMap<String, String>,
 }
 
 fn main() -> anyhow::Result<()> {
-    // let conf_path =
-    //     std::env::var("CONFIG_PATH").unwrap_or_else(|_| "/etc/bridgehead/conf.toml".into());
-    // let conf: Config = toml::from_str(&std::fs::read_to_string(conf_path)?)?;
-    let conf = Config {
-        project: Project::Ccp,
-        http_proxy_url: None,
-        https_proxy_url: None,
-        site_id: "test".into(),
-    };
+    let conf_path =
+        std::env::var("CONFIG_PATH").unwrap_or_else(|_| "/etc/bridgehead/conf.toml".into());
+    let conf: Config = toml::from_str(&std::fs::read_to_string(conf_path)?)?;
 
     let mut services = dep_map::ServiceMap::default();
-    match conf.project {
-        Project::Ccp => {
-            services.install::<Focus<DktkBroker>>(&conf);
-            modules::CCP_MODULES
-                .iter()
-                .for_each(|&m| services.install_module(m, &conf));
-            services.write_composables()?
-        }
-        Project::Bbmri => todo!(),
-        Project::Minimal => todo!(),
-    }
+    services.install::<Focus<DktkBroker>>(&conf);
+    modules::CCP_MODULES
+        .iter()
+        .for_each(|&m| services.install_module(m, &conf));
+    services.write_composables()?;
     Ok(())
 }
