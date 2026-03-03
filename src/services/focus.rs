08970463d8402c5cd9a3901754758c8c5ccdcde4
Enola Knezevic <enola.knezevic@dkfz-heidelberg.de>
2026-03-03 15:03:31 +0100

    added EUCAIM

--- src/services/focus.rs
+++ src/services/focus.rs
@@ -3,7 +3,10 @@ use std::marker::PhantomData;
 use askama::Template;
 use url::Url;
 
-use crate::services::BeamAppInfos;
+use crate::{
+    modules::{EucaimConfig, EucaimEndpointType},
+    services::BeamAppInfos,
+};
 
 use super::{
     Deps, Service,
@@ -18,12 +21,16 @@ where
     Self: Service,
 {
     beam: BeamAppInfos,
-    blaze_url: Url,
+    endpoint_url: Option<Url>,
     endpoint_type: String,
     tag: String,
     /// (exporter_url, exporter_api_key)
     exporter: Option<(String, String)>,
     beam_and_blaze: PhantomData<(Beam, Backend)>,
+    /// (provider, provider_icon)
+    provider_data: Option<(String, String)>,
+    auth_header: Option<String>,
+    postgres_connection_string: Option<String>,
 }
 
 impl<T: BrokerProvider, B: BlazeProvider> Service for Focus<T, Blaze<B>> {
@@ -38,10 +45,44 @@ impl<T: BrokerProvider, B: BlazeProvider> Service for Focus<T, Blaze<B>> {
         Focus {
             beam,
             beam_and_blaze: PhantomData,
-            blaze_url: Blaze::<B>::get_url(),
+            endpoint_url: Some(Blaze::<B>::get_url()),
             tag: tag.clone(),
             endpoint_type: "blaze".into(),
             exporter: None,
+            provider_data: None,
+            auth_header: None,
+            postgres_connection_string: None,
+        }
+    }
+
+    fn service_name() -> String {
+        format!("{}-focus", T::network_name())
+    }
+}
+
+// only going to be used for Eucaim
+impl<T: BrokerProvider> Service for Focus<T, EucaimEndpointType> {
+    type Dependencies = (BeamProxy<T>,);
+    type ServiceConfig = EucaimConfig;
+
+    fn from_config(config: Self::ServiceConfig, (beam_proxy,): Deps<Self>) -> Self
+    where
+        Self: Sized,
+    {
+        let beam = beam_proxy.add_service("focus");
+        Focus {
+            beam,
+            beam_and_blaze: PhantomData,
+            endpoint_url: config.endpoint_url,
+            tag: match config.endpoint_type {
+                EucaimEndpointType::EucaimSql => "develop-query-sql".to_string(),
+                _ => "develop".to_string(),
+            },
+            endpoint_type: serde_json::to_string(&config.endpoint_type).unwrap(),
+            exporter: None,
+            provider_data: Some((config.provider, config.provider_icon)),
+            auth_header: config.auth_header,
+            postgres_connection_string: config.postgres_connection_string,
         }
     }
 
