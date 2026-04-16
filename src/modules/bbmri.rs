90ce66bfde68177610d008534044d98f0d62cf52
Skiba Jan <jan.skiba@dkfz-heidelberg.de>
2026-04-16 14:52:42 +0200

    fix fmt and snapshots

--- src/modules/bbmri.rs
+++ src/modules/bbmri.rs
@@ -30,8 +30,7 @@ impl Module for Bbmri {
         service_map.install_default::<Blaze<Self>>();
         if bbmri_conf.eric {
             if let Environment::Acceptance = conf.environment {
-                service_map
-                    .install_with_config::<Focus<EricAcc, Blaze<Self>>>("develop".into());
+                service_map.install_with_config::<Focus<EricAcc, Blaze<Self>>>("develop".into());
             } else {
                 service_map.install_with_config::<Focus<Eric, Blaze<Self>>>("main".into());
             }
